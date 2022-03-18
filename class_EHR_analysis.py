e3b4c83478949f55b3b425aa27c945087cee6f72
Gloria Gu <zg92@duke.edu>
2022-03-18 14:20:02 -0400

    Updating requested changes

--- class_EHR_analysis.py
+++ class_EHR_analysis.py
@@ -4,25 +4,25 @@ from datetime import datetime
 class Patient:
     def __init__(self, list_data):
         """Initial function"""
-        self.ID = list_data[0]
+        self.id = list_data[0]
         self.gender = list_data[1]
-        self.DOB = datetime.strptime(list_data[2], "%Y-%m-%d %H:%M:%S.%f")
+        self.dob = datetime.strptime(list_data[2], "%Y-%m-%d %H:%M:%S.%f")
         self.race = list_data[3]
 
     @property
     def age(self):
         """A property age"""
-        return round(float((datetime.now() - self.DOB).days) / 365, 2)
+        return round(float((datetime.now() - self.dob).days) / 365, 2)
 
 
 class Lab:
     def __init__(self, list_data):
-        self.ID = list_data[0]
+        self.id = list_data[0]
         self.value = list_data[3]
         self.units = list_data[4]
 
 
-def build_data_patient(file_name):
+def build_data_patient(file_name: str) -> list[list[str]]:
     data = []
     with open(file_name, "r", encoding="utf-8-sig") as files:
         for line in files.readlines()[1:]:
@@ -30,7 +30,7 @@ def build_data_patient(file_name):
     return data
 
 
-def build_data_lab(file_name):
+def build_data_lab(file_name: str) -> list[list[str]]:
     data = []
     with open(file_name, "r", encoding="utf-8-sig") as files:
         for line in files.readlines()[1:]:
@@ -49,4 +49,3 @@ if __name__ == "__main__":
             "/Users/guzhengyi/Desktop/BIOSTAT 821/LabsCorePopulatedTable.txt"
         )[0].units
     )
-
